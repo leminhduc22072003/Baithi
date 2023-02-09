acb4518acf4ca21c06e78f4ad0179265987325a5
leminhduc22072003 <114459673+leminhduc22072003@users.noreply.github.com>
2023-02-09 09:52:12 +0700

    baithi1

--- Baithi/Program.cs
+++ Baithi/Program.cs
@@ -1,1 +1,20 @@
-﻿using System 
+﻿using System;
+
+namespace GeometryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius of cylinder:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter height of cylinder:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Cylinder cylinder = new Cylinder(radius, height);
+            cylinder.Process();
+            Console.WriteLine("Cylinder Result:");
+            cylinder.Result();
+        }
+    }
+}
